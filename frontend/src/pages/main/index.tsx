684fbf112dc4c7e27c4a2018c9bbf5f3a8d221ec
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-05-05 10:30:43 +0200

    trying to make the design

--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -2,6 +2,7 @@
 import Button from "../../components/Button";
 import styles from "./index.module.css"
 import React, { useEffect, useState } from 'react';
+import "./index.css"
 
 
 export default function MainPage(){
@@ -11,10 +12,52 @@ export default function MainPage(){
         .then((response) => response.json())
         .then((pokemon) => {
             console.log(pokemon)
-
+            console.log(pokemon.pokemon.name)
+            setPokemonName(pokemon.pokemon.name)
         }); 
     },[])
     return(
-        <div>{pokemonName}s</div>
+        <>
+          <div className="section">
+            <h1>Stair Edge Section</h1>
+        </div>
+        </>
     )
 }
+/*
+
+import Button from "../../components/Button";
+import styles from "./index.module.css"
+import React, { useEffect, useState } from 'react';
+import "./index.css"
+
+
+export default function MainPage(){
+    const [pokemonName, setPokemonName] = useState("")
+    useEffect(()=>{
+        fetch('http://localhost:5000/api/randompokemon')
+        .then((response) => response.json())
+        .then((pokemon) => {
+            console.log(pokemon)
+            console.log(pokemon.pokemon.name)
+            setPokemonName(pokemon.pokemon.name)
+        }); 
+    },[])
+    return(
+        <>
+        /
+        <div>{pokemonName}s</div>
+        <div className="center">
+            <div className="mainBlock">
+                <div className="mainHeader"></div>
+                <div className="mainContent">
+                    <div className="card"></div>
+                </div>
+            </div>
+            <div className="sideBlock"></div>
+        </div>
+        </>
+    )
+}
+
+*/

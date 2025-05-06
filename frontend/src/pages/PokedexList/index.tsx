c071166a782adefdc930e937894c0301e8bbdc85
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-05-07 00:15:39 +0200

    first part of lazyloading - page jumps from 1 to 3

--- frontend/src/pages/PokedexList/index.tsx
+++ frontend/src/pages/PokedexList/index.tsx
@@ -1,19 +1,71 @@
 import Button from "../../components/Button";
 import styles from "./index.module.css"
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import "./index.css"
 import { url } from "inspector";
 import { useQuery } from '@tanstack/react-query';
 
 
+export interface pPokemonListResult {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: {
+      name: string;
+      url: string;
+    }[];
+    page?: number; 
+  }
 
+
+
+
+  interface Pokemon {
+    name: string;
+    url: string;
+  }
+  
+  // Type for the response structure from the API (only interested in results here)
+  // Type for the response structure from the API (only interested in results here)
+  interface PokemonListResult {
+    data:{
+        results: Pokemon[];
+        page: number
+    };
+  }
+  
 
 export default function PokedexList(){
+    const [items, setItems] = useState<Pokemon[]>([]);
+    const [page, setPage] = useState<number>(1)
+
+
+    const loadPokemon = async () =>{
+        const res = await axios.get<PokemonListResult>(`http://localhost:5000/api/pokedex?page=${page}&limit=10`)
+        console.log(res.data)
+        console.log(page)
+        console.log(res.data.data.results)
+
+        if (items.length >0) {
+            setItems([...items, ...res.data.data.results]);
+          } else {
+            setItems(res.data.data.results);
+          }
+      
+
+          // if the page isnt set update it based on the page in state
+          if (res.data.data.page) {
+            setPage(res.data.data.page);
+          } else {
+            setPage(prev => prev + 1);
+          }
+    }
+
+
+
     useEffect(()=>{
-        fetch("http://localhost:5000/api/pokedex?page=2&limit=10")
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+        loadPokemon()
     },[])
 
 
@@ -28,7 +80,7 @@ export default function PokedexList(){
                     <div className="card">
                         <p>sds</p>
                     </div>
-
+                    <Button onClick={()=>loadPokemon()}/>
 
 
                 </div>

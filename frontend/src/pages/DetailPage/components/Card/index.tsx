2e214c4d244f94be61aacae66b3ec689b372953e
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-05-08 08:35:00 +0200

    added type images to the detail card

--- frontend/src/pages/DetailPage/components/Card/index.tsx
+++ frontend/src/pages/DetailPage/components/Card/index.tsx
@@ -24,14 +24,14 @@ export default function Card({data}: PokemonDetailCardProps){
         fairy: "#D685AD"
       };
       
-    
+    console.log(`url(../../../assets/typeBanners/${data.type}.png` )
     return(
         <div className="card">
 
         <div className="cardTitle">
                 <h2>{data.name}</h2>
             <div className="cardType">
-                <div className="typeImage"></div>
+                <div className="typeImage"  style={{ backgroundImage: `url(/assets/typeBanners/${data.type}.png` }}></div>
                 <div className="typeName">
                     <p>{data.type}</p>
                 </div>

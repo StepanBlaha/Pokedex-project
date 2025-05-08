d9a4360fa72214ca5c760fe802535b88ad9d3e33
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-05-09 01:01:42 +0200

    design work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,25 @@ import DetailPage from './pages/DetailPage';
 import PokedexList from './pages/PokedexList';
 import AppRoutes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+      <>
+
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Helmet>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </>
   );
 }
 

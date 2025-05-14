ce116498499b0ac9a410af41749aeee1dad30dbd
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-05-14 18:30:54 +0200

    item types

--- backend/src/services/itemService.ts
+++ backend/src/services/itemService.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Item } from '../types/itemTypes';
 
 export const getItemList = async(page: number, limit: number) =>{
     const pageNum = page || 0 ;
@@ -8,6 +9,6 @@ export const getItemList = async(page: number, limit: number) =>{
     return res.data;
 }
 export const getItemDetails = async(id:number) =>{
-    const res = await axios.get(`https://pokeapi.co/api/v2/item/${id}`)
+    const res = await axios.get<Item>(`https://pokeapi.co/api/v2/item/${id}`)
     return res.data;
 }
--- backend/src/types/itemTypes.ts
+++ backend/src/types/itemTypes.ts
@@ -1,5 +1,32 @@
-
-
 export interface Item{
-    
+    attributes: NamedResource[];
+    baby_trigger_for: string | null;
+    category: NamedResource;
+    cost: number;
+    effect_entries: EffectEntry[];
+    flavor_text_entries: FlavorTextEntry[];
+    name: string;
+    id: number;
+    sprites: Sprite;
+}
+interface FlavorTextEntry{
+    language: NamedResource;
+    text: string;
+    version_group: NamedResource;
+
+}
+interface EffectEntry{
+    effect: string;
+    language: NamedResource;
+    short_effect: string;
+}
+
+// Sprite type
+interface Sprite{
+    default: string | null
+}
+// Base type for named data
+interface NamedResource{
+    name: string;
+    url: string;
 }

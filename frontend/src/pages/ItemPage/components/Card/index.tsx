236171c78ad5e5498ffb8ccfb279b78a7725cce9
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-05-15 20:25:49 +0200

    move card tooltip fix

--- frontend/src/pages/ItemPage/components/Card/index.tsx
+++ frontend/src/pages/ItemPage/components/Card/index.tsx
@@ -34,8 +34,8 @@ const Card = forwardRef<HTMLDivElement, ItemProps>(({ name, id, attributes, cate
         </div>
 
         <div className={styles.tooltipContainer}>
-            <span className={styles.tooltip}>{desc}</span>
             <span className={styles.text}>
+            <span className={styles.tooltip}>{desc}</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-info-icon lucide-info"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>
             </span>
         </div>

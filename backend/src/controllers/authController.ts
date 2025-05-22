37cc75c02797ecbc84e9762f7379b1c291a6a1a7
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-05-22 10:03:30 +0200

    egister form design + backend work

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -3,10 +3,10 @@ import * as userService from '../services/authService'
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
-        const user = userService.createUser(req.body)
+        const user = await userService.createUser(req.body)
         res.status(201).json(user)
     } catch(error: any) {
+        console.log("Error: "+ error.message)
         res.status(400).json({error: error.message})
     }
 }
-  
--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -2,7 +2,11 @@ import User from "../models/Users"
 
 export const createUser = async (data: any) => {
     const userExists = await User.findOne({ email: data.email });
-    if (userExists) throw new Error('Email is already registered');
+    if (userExists){
+        throw new Error('Email is already registered');
+        return "Email is already registered"
+    }
+        
 
     const newUser = new User(data);
     return await newUser.save();

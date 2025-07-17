d2ad19682845de1043e1a237f73debb444fe8bc6
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-07-17 19:28:00 +0200

    er

--- frontend/src/context/pokemonContext.tsx
+++ frontend/src/context/pokemonContext.tsx
@@ -2,7 +2,6 @@ import React, { createContext, useState, useEffect, useContext } from "react";
 import { PokemonListResult, Pokemon, PokemonContextType } from "../types/pokemon";
 import { getCachedData } from "../utils/cache";
 import axios from "axios";
-const API_BASE_URL = "https://pokelog-production.up.railway.app";
 const PokemonContext = createContext<PokemonContextType>({
     pokemon: [],
     loading: true
@@ -10,6 +9,8 @@ const PokemonContext = createContext<PokemonContextType>({
 
 // Load all pokemon
 const fetchAllPokemonInBatches = async () => {
+    const API_BASE_URL = "https://pokelog-production.up.railway.app";
+    console.log(API_BASE_URL)
     const cache = getCachedData("sb_pokemon");
     if(cache !== null) return JSON.parse(cache);
     const limit = 100;
